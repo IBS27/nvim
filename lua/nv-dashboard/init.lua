1be77e9ab82d7baa8734d3f7b7a2a8bef620f6cd
IBS27 <ibsrinivas27@gmail.com>
2021-07-23 11:13:37 +0530

    Made some changes to dashboard

--- lua/nv-dashboard/init.lua
+++ lua/nv-dashboard/init.lua
@@ -3,50 +3,40 @@ local g = vim.g
 g.dashboard_default_executive = "telescope"
 
 g.dashboard_custom_section = {
-    a = {
-        description = {"      Find File          "},
-        command = "Telescope find_files"
-    },
-    b = {
-        description = {"      Recently Used Files"},
-        command = "Telescope oldfiles"
-    },
-    c = {
-        description = {"      Find Word          "},
-        command = "Telescope live_grep"
-    },
-    d = {
-        description = {"      Settings           "},
-        command = ":e ~/.config/nvim/init.lua"
-    }
+  a = {
+    description = { "      Find File          " },
+    command = "Telescope find_files",
+  },
+  b = {
+    description = { "      Recently Used Files" },
+    command = "Telescope oldfiles",
+  },
+  c = {
+    description = { "      Find Word          " },
+    command = "Telescope live_grep",
+  },
+  d = {
+    description = { "      Settings           " },
+    command = ":e ~/.config/nvim/init.lua",
+  },
 }
 
---[[
 g.dashboard_custom_header = {
   "",
-  "                                               ",
-  "        ███████████           █████      ██",
-  "       ███████████             █████ ",
-  "       ████████████████ ███████████ ███   ███████",
-  "      ████████████████ ████████████ █████ ██████████████",
-  "     █████████████████████████████ █████ █████ ████ █████",
-  "   ██████████████████████████████████ █████ █████ ████ █████",
-  "  ██████  ███ █████████████████ ████ █████ █████ ████ ██████",
-  "  ██████   ██  ███████████████   ██ █████████████████",
-  "  ██████   ██  ███████████████   ██ █████████████████ ",
-  ""
-}
---
-]]
-g.dashboard_custom_header = {
-    "",
-    " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
-    " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
-    " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
-    " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-    " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-    " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
-    ""
+  " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+  " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+  " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+  " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+  " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+  " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+  "",
 }
 
-g.dashboard_custom_footer = {"Github:  IBS27"}
+vim.cmd "let packages = len(globpath('~/.local/share/nvim/site/pack/packer/start', '*', 0, 1))"
+
+vim.api.nvim_exec(
+  [[
+    let g:dashboard_custom_footer = ['LuaJIT loaded '..packages..' plugins']
+]],
+  false
+)
