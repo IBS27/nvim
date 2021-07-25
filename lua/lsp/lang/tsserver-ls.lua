6ef889fa4073fa765d9f50e02054e15d97b2b888
IBS27 <ibsrinivas27@gmail.com>
2021-07-25 12:40:55 +0530

    Minor changes

--- lua/lsp/lang/tsserver-ls.lua
+++ lua/lsp/lang/tsserver-ls.lua
@@ -1,5 +1,1 @@
-require("lspconfig").tsserver.setup {
-  cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-  -- root_dir = root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
-}
+require("lspconfig").tsserver.setup {}
