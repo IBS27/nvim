980443b56ef54b11b59e1585a7cf2d5e1bd4bc69
IBS27 <ibsrinivas27@gmail.com>
2021-07-30 11:37:29 +0530

    Minor changes to whichkey

--- lua/nv-whichkey/init.lua
+++ lua/nv-whichkey/init.lua
@@ -22,7 +22,7 @@ M.config = {
   window = {
     border = "none", -- none, single, double, shadow
     position = "bottom", -- bottom, top
-    margin = { 1, 1, 1, 1 }, -- extra window margin [top, right, bottom, left]
+    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
   },
   layout = {
