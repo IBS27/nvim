b6fb306ea8c5530641fdb14bb738d2265d6f0f81
IBS27 <ibsrinivas27@gmail.com>
2021-07-31 20:44:35 +0530

    Changed HTML formatter to format jinja template tags

--- lua/nv-formatter/init.lua
+++ lua/nv-formatter/init.lua
@@ -14,7 +14,15 @@ M.config = {
   filetype = {
     javascript = { prettier },
     typescript = { prettier },
-    html = { prettier },
+    html = {
+      function()
+        return {
+          exe = "prettydiff",
+          args = { "beautify", vim.api.nvim_buf_get_name(0), "options", "indent_size=2" },
+          stdin = true,
+        }
+      end,
+    },
     css = { prettier },
     scss = { prettier },
     markdown = { prettier },
