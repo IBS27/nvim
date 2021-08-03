5949147518739f07f570d99eacf91bfd8a2f8757
IBS27 <ibsrinivas27@gmail.com>
2021-08-03 18:46:06 +0530

    minor change

--- lua/settings.lua
+++ lua/settings.lua
@@ -28,6 +28,7 @@ opt.ignorecase = true -- ignore case in search patterns
 opt.mouse = "a" -- allow the mouse to be used in neovim
 opt.pumheight = 10 -- pop up menu height
 opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
+opt.showcmd = false -- previous command is hidden
 opt.showtabline = 2 -- always show tabs
 opt.smartcase = true -- smart case
 opt.autoindent = true -- Good auto indent
