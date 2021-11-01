a1f7e98c78807b47b68efd964c5cc4d56ff11491
IBS27 <ibsrinivas27@gmail.com>
2021-11-01 13:37:47 +0530

    made some changes to nvimtree

--- lua/nv-nvimtree/init.lua
+++ lua/nv-nvimtree/init.lua
@@ -1,22 +1,11 @@
-local M = {}
-
 require("nvim-tree").setup {
   side = "left",
-  show_icons = {
-    git = true,
-    folders = true,
-    files = true,
-    tree_width = 30,
-  },
-  -- ignore = { ".git", "node_modules", ".cache", ".DS_Store" },
-  gitignore = false,
   auto_open = false,
   auto_close = true,
   quit_on_open = false,
   update_focused_file = {
     enable = true,
   },
-  hide_dotfiles = false,
   git_hl = true,
   root_folder_modifier = ":t",
   tab_open = false,
@@ -24,10 +13,11 @@ require("nvim-tree").setup {
   diagnostics = {
     enable = true,
   },
-  ignore_ft_on_setup = { ".git", "node_modules", ".cache", ".DS_Store" },
+  filters = {
+    custom = { ".git", "node_modules", ".cache", ".DS_Store" },
+  },
   auto_ignore_ft = { "startify", "dashboard" },
   hijack_cursor = true,
-  indent_markers = true,
   icons = {
     default = "",
     symlink = "",
@@ -56,23 +46,4 @@ vim.g["nvim_tree_show_icons"] = {
   folder_arrows = 0,
 }
 
-M.toggle_tree = function()
-  local view_status_ok, view = pcall(require, "nvim-tree.view")
-  if not view_status_ok then
-    return
-  end
-  if view.win_open() then
-    require("nvim-tree").close()
-    if package.loaded["bufferline.state"] then
-      require("bufferline.state").set_offset(0)
-    end
-  else
-    if package.loaded["bufferline.state"] then
-      -- require("bufferline.state").set_offset(31, "File Explorer")
-      require("bufferline.state").set_offset(30, "")
-    end
-    require("nvim-tree").toggle()
-  end
-end
-
-return M
+vim.g["nvim_tree_indent_markers"] = 1
