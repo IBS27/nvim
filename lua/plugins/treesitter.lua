0b2cb75e305f0120feae7d2aeff05ec89856cd53
IBS27 <ibsrinivas27@gmail.com>
2022-02-05 20:45:33 +0530

    minor change

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ require("nvim-treesitter.configs").setup {
     additional_vim_regex_highlighting = true,
   },
   indent = {
-    enable = false,
+    enable = true,
   },
   autotag = {
     enable = true,
