f694e0f0d3a9e461d6728584da3d88464c6c78c9
IBS27 <ibsrinivas27@gmail.com>
2023-05-13 21:45:21 +0530

    added html lsp support

--- lua/lsp/lsp-installer.lua
+++ lua/lsp/lsp-installer.lua
@@ -3,3 +3,10 @@ require("mason-lspconfig").setup()
 
 require("lspconfig").lua_ls.setup {}
 require("lspconfig").pyright.setup {}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require('lspconfig').html.setup {
+    capabilities = capabilities,
+}
