6b122c9860a1717051681b63fd52725382e8634c
IBS27 <ibsrinivas27@gmail.com>
2023-05-14 12:33:24 +0530

    added css lsp and completion

--- lua/lsp/lsp-installer.lua
+++ lua/lsp/lsp-installer.lua
@@ -10,3 +10,6 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 require('lspconfig').html.setup {
     capabilities = capabilities,
 }
+require 'lspconfig'.cssls.setup {
+    capabilities = capabilities,
+}
