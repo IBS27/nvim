f6609c5ac263bfbed0aed8a259f102bbee353d5f
Srinivas I B <srinivasib@Srinivass-MacBook-Pro.local>
2024-03-19 18:28:02 +0530

    added html and css support with ts-autotags

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -2,13 +2,17 @@ local M = {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPost", "BufNewFile" },
   build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
 }
 
 function M.config()
   require("nvim-treesitter.configs").setup {
-    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" },
+    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "html", "css" },
     highlight = { enable = true },
     indent = { enable = true },
+    autotag = { enable = true },
   }
 end
 
