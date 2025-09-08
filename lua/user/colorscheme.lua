f62079a7a6c4448504384feab9baec01984891d8
Srinivas I B <ibsrinivas27@gmail.com>
2025-09-08 12:00:50 -0500

    updated colorscheme from gruvbox to gruvbox-material

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,31 +1,11 @@
 local M = {
-  -- "Mofiqul/vscode.nvim",
-  "ellisonleao/gruvbox.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000
 }
 
 function M.config()
-  -- require("vscode").setup {
-  --   disable_nvimtree_bg = true,
-  --   underline_links = true,
-  --   terminal_colors = true,
-  --
-  --   color_overrides = {
-  --     vscCursorDarkDark = "#252525",
-  --   }
-  -- }
-
-  require("gruvbox").setup({
-    overrides = {
-      SignColumn = {bg = "#282828"},
-      FoldColumn = {bg = "#282828"},
-      StatusLineNC = {bg = "#282828"}
-    }
-  })
-
-  -- vim.cmd.colorscheme "vscode"
-  vim.cmd.colorscheme "gruvbox"
+  vim.cmd.colorscheme "gruvbox-material"
 end
 
 return M
