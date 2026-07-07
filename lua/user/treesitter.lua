b9ea74f8517627a2f989beb5c17f9e84e2b215b9
Srinivas I B <ibsrinivas27@gmail.com>
2026-07-07 12:20:15 -0500

    Fix treesitter setup for Neovim 0.12

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,18 +1,88 @@
 local M = {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   dependencies = {
     "windwp/nvim-ts-autotag",
   },
 }
 
+local parsers = {
+  "bash",
+  "c",
+  "cpp",
+  "css",
+  "html",
+  "java",
+  "javascript",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "tsx",
+  "typescript",
+}
+
+local highlight_filetypes = {
+  "bash",
+  "c",
+  "cpp",
+  "css",
+  "html",
+  "java",
+  "javascript",
+  "javascriptreact",
+  "lua",
+  "markdown",
+  "python",
+  "sh",
+  "typescript",
+  "typescriptreact",
+}
+
+local indent_filetypes = {
+  "bash",
+  "c",
+  "cpp",
+  "css",
+  "html",
+  "java",
+  "javascript",
+  "javascriptreact",
+  "lua",
+  "python",
+  "sh",
+  "typescript",
+  "typescriptreact",
+}
+
 function M.config()
-  require("nvim-treesitter.configs").setup {
-    ensure_installed = { "c", "cpp", "lua", "markdown", "markdown_inline", "bash", "python", "html", "css", "java", "javascript", "typescript", "tsx" },
-    highlight = { enable = true },
-    indent = { enable = true },
+  local treesitter = require "nvim-treesitter"
+
+  treesitter.setup {
+    install_dir = vim.fn.stdpath "data" .. "/site",
   }
+  treesitter.install(parsers)
+
+  local group = vim.api.nvim_create_augroup("user_treesitter", { clear = true })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    pattern = highlight_filetypes,
+    callback = function(args)
+      pcall(vim.treesitter.start, args.buf)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    pattern = indent_filetypes,
+    callback = function(args)
+      vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  })
+
   require("nvim-ts-autotag").setup({})
 end
 
